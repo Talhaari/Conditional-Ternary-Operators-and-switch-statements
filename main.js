f77805ccc55a04e98f0f4ea0f3f09e25434896b4
Talha arif <160534871+Talhaari@users.noreply.github.com>
2024-03-19 00:54:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// switch statements
+var dayofweek = "tuesday";
+switch (dayofweek) {
+    case "wednesday":
+        console.log("go to market");
+        break;
+    case "monday":
+        console.log("read a book");
+        break;
+    case "tuesday":
+        console.log("cleaning room");
+        break;
+    case "wednesday":
+        console.log("planting");
+    default:
+        console.log("make a typescript program");
+}
---
+++ main.ts
@@ -0,0 +1,15 @@
+// switch statements
+// let dayofweek = "tuesday"
+// switch(dayofweek) {
+    // case "wednesday":
+    // console.log("go to market")
+    // break
+    // case "monday":
+        // console.log("read a book")
+        // break
+        // case "tuesday" :
+            // console.log("cleaning room")
+            // break
+        // default:
+            // console.log("make a typescript program")
+// }
